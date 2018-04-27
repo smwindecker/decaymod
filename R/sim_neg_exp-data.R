fdfb25058a85e83db3d39f9288dc7644319d0955
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-04-27 16:29:38 +0200

    create simulated datasets

---
+++ R/sim_neg_exp-data.R
@@ -0,0 +1,15 @@
+#' Simulated Negative Exponential data
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage data(sim_neg_exp)
+#'
+#' @format An object of class \code{'cross'}
+#'
+#' @keywords datasets
+#'
+#' @source Saras M Windecker
+
+'sim_neg_exp'
---
+++ R/sim_weibull-data.R
@@ -0,0 +1,15 @@
+#' Simulated Weibull data
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage data(sim_weibull)
+#'
+#' @format An object of class \code{'cross'}
+#'
+#' @keywords datasets
+#'
+#' @source Saras M Windecker
+
+'sim_weibull'
