334c7f9790900a0f5bc028fffceeaa538b7e1425
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-10-09 11:41:29 -0400

    add decay data as data to package

---
+++ R/decay-data.R
@@ -0,0 +1,16 @@
+#' Simulated negative exponential data
+#'
+#' @docType data
+#'
+#' @usage data(decay)
+#'
+#' @format A dataframe
+#'
+#' @keywords datasets
+#'
+#' @source Saras Windecker
+#'
+#' @examples
+#' data(decay)
+#' head(decay)
+"decay"
