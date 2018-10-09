e1728e1d49a7b102bab08dd8bf98c7a294f7d0f7
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-10-09 11:44:30 -0400

    add option for testing of not CV model

--- R/ne_noCV_RE_stan.R
+++ R/ne_noCV_RE_stan.R
@@ -7,11 +7,14 @@
 #' @param time Vector of time values for training data
 #' @param sp Numeric group identifier
 #' @param J Number of groups
-#' @param X Model matrix for training data
+#' @param X Model matrix for training data#'
+#' @param m0_sim Simulated log initial mass data
+#' @param time_sim Simulated time data
+#' @param sp_sim Numeric group identifier for sim dataset
 #' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
 #' @return An object of class `stanfit` returned by `rstan::sampling`
 #'
-ne_noCV_RE_stan <- function(mT, m0, time, sp, J, X, ...) {
+ne_noCV_RE_stan <- function(mT, m0, time, sp, J, X, m0_sim, time_sim, sp_sim,...) {
   stan_data <- list(mT = mT,
                     m0 = m0,
                     time = time,
@@ -19,7 +22,11 @@ ne_noCV_RE_stan <- function(mT, m0, time, sp, J, X, ...) {
                     sp = sp,
                     J = J,
                     X = X,
-                    P = ncol(X))
+                    P = ncol(X),
+                    m0_sim = m0_sim,
+                    time_sim = time_sim,
+                    N_sim = length(time_sim),
+                    sp_sim = sp_sim)
   out <- rstan::sampling(stanmodels$ne_noCV_RE, data = stan_data,
                          control = list(adapt_delta = 0.99, max_treedepth = 15), ...)
   return(out)
